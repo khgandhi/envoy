8313b582286554c2344fe40f652dd09a8ac3268b
Jose Nino <jnino@lyft.com>
2017-05-02 13:51:35 -0700

    test name

--- ci/build_container/update_build_container.sh
+++ ci/build_container/update_build_container.sh
@@ -3,7 +3,8 @@ set -e
 
 cd ci/build_container
 ./docker_build.sh
-docker push lyft/envoy-build:$TRAVIS_COMMIT
-docker tag lyft/envoy-build:$TRAVIS_COMMIT lyft/envoy-build:latest
-docker push lyft/envoy-build:latest
+# docker push lyft/envoy-build:$TRAVIS_COMMIT
+docker tag lyft/envoy-build:$TRAVIS_COMMIT lyft/envoy-build-test:latest
+# docker push lyft/envoy-build:latest
+docker push lyft/envoy-build-test:latest
 cd ../../
